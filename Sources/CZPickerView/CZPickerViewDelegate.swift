261a0fb0262de80d0cec1b67ed11fa3123186b3f
truongdv <truongdv@sapo.vn>
2025-02-07 11:53:15 +0700

    add library

---
+++ Sources/CZPickerView/CZPickerViewDelegate.swift
@@ -0,0 +1,52 @@
+//
+//  CZPickerViewDelegate.swift
+//
+//  Created by Kien Nguyen on 05/06/2021.
+//
+
+import Foundation
+import UIKit
+
+public protocol CZPickerViewDelegate: AnyObject {
+
+    /** delegate method for picking one item */
+    func czpickerView(_ pickerView: CZPickerView, didConfirmWithItemAtRow row: Int)
+    
+    func czpickerView(_ pickerView: CZPickerView, didSelected row: Int)
+
+    /**
+     delegate method for picking multiple items,
+     implement this method if `allowMultipleSelection` is true
+     */
+    func czpickerView(_ pickerView: CZPickerView, didConfirmWithItemsAtRows rows: [Int])
+
+    /** delegate method for clicking confirm button */
+    func czpickerViewDidClickConfirmButton(_ pickerView: CZPickerView)
+
+    /** delegate method for canceling */
+    func czpickerViewDidClickCancelButton(_ pickerView: CZPickerView)
+
+    /** delegate method for searching from search field */
+    func czpickerView(_ pickerView: CZPickerView, searching searchText: String)
+
+    func czpickerViewDidDismissWhenTappingOutSide()
+
+}
+
+/** Default implementations */
+public extension CZPickerViewDelegate {
+    
+    func czpickerView(_ pickerView: CZPickerView, didConfirmWithItemAtRow row: Int) {}
+    
+    func czpickerView(_ pickerView: CZPickerView, didSelected row: Int) {}
+    
+    func czpickerView(_ pickerView: CZPickerView, didConfirmWithItemsAtRows rows: [Int]) {}
+    
+    func czpickerViewDidClickConfirmButton(_ pickerView: CZPickerView) {}
+    
+    func czpickerViewDidClickCancelButton(_ pickerView: CZPickerView) {}
+    
+    func czpickerView(_ pickerView: CZPickerView, searching searchText: String) {}
+    
+    func czpickerViewDidDismissWhenTappingOutSide() {}
+}
